0ff41255909e56c34675e2cdd8e61333044691b2
bhumikarbsc24-blip <bhumikarbsc24@rvu.edu.in>
2025-11-10 10:39:22 +0530

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -37,4 +37,44 @@ try {
                     else if (guess > number) {
                         JOptionPane.showMessageDialog(null, " Too high! Try again. (Try " + tries + ")");
                     } 
+                    else {
+                        JOptionPane.showMessageDialog(null,
+                            " Correct! You got it in " + tries + " tries!\nThe secret number was: " + number,
+                            "Winner!", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                } 
+                catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, " Please enter a valid number!");
+                }
+            }
 
+            // Update scoreboard
+            totalGames++;
+            totalTries += tries;
+            if (tries < bestScore) {
+                bestScore = tries;
+            }
+
+            // Show scoreboard
+            double average = (double) totalTries / totalGames;
+            String scoreboard = " Scoreboard \n"
+                + "Games Played: " + totalGames + "\n"
+                + "Total Tries: " + totalTries + "\n"
+                + "Average Tries: " + String.format("%.2f", average) + "\n"
+                + "Best Score (Least Tries): " + bestScore;
+
+            JOptionPane.showMessageDialog(null, scoreboard, "Scoreboard", JOptionPane.INFORMATION_MESSAGE);
+
+            // Ask to play again
+            int option = JOptionPane.showConfirmDialog(null,
+                "Do you want to play again?", "Play Again?", JOptionPane.YES_NO_OPTION);
+
+            if (option != JOptionPane.YES_OPTION) {
+                JOptionPane.showMessageDialog(null,
+                    "Thanks for playing! \n\nFinal Scoreboard:\n" + scoreboard,
+                    "Goodbye!", JOptionPane.INFORMATION_MESSAGE);
+                playAgain = false;
+            }
+        }
+    }
+}
